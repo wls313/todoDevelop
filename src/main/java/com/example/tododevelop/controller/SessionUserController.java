f6b46c351dfe063a3bc7acd3bbd4425b8a23b458
ww313 <wlsghddl3358@naver.com>
2024-12-18 23:16:36 +0900

    세션 확인

---
+++ src/main/java/com/example/tododevelop/controller/SessionUserController.java
@@ -0,0 +1,56 @@
+package com.example.tododevelop.controller;
+
+import com.example.tododevelop.dto.LoginRequestDto;
+import com.example.tododevelop.dto.LoginResponseDto;
+import com.example.tododevelop.dto.UserResponseDto;
+import com.example.tododevelop.entity.Const;
+import com.example.tododevelop.entity.Member;
+import com.example.tododevelop.service.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@Controller
+@RequiredArgsConstructor
+public class SessionUserController {
+
+    private final UserService userService;
+
+    @PostMapping("/session-login")
+    public ResponseEntity<LoginResponseDto> login(
+            @Valid @RequestBody LoginRequestDto dto,
+            HttpServletRequest request
+    ) {
+
+        LoginResponseDto responseDto = userService.login(dto.getEmail(), dto.getPassword());
+        Long userId = responseDto.getId();
+
+        // 실패시 예외처리
+        if (userId == null) {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+        // 로그인 성공시 로직
+        // Session의 Default Value는 true이다.
+        // Session이 request에 존재하면 기존의 Session을 반환하고,
+        // Session이 request에 없을 경우에 새로 Session을 생성한다.
+        HttpSession session = request.getSession();
+
+        // 회원 정보 조회
+        Optional<Member> loginUser = userService.findById(userId);
+
+        // Session에 로그인 회원 정보를 저장한다.
+        session.setAttribute(Const.LOGIN_USER, loginUser);
+
+        // 로그인 성공시 리다이렉트
+        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+    }
+}
